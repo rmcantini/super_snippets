9d3b75bc6ca00de6d18d7fd138200ef30cf85359
rmcantini <rmcantini@gmail.com>
2023-08-09 13:25:46 -0300

    padx fixed for mac

--- snipets_txt_gui2.py
+++ snipets_txt_gui2.py
@@ -17,9 +17,9 @@ label = tk.Label(root, text="Start or Stop")
 label.pack(padx=8, pady=8)
 
 start_button = tk.Button(root, text="Start", command=start_script)
-start_button.pack(side="left", padx=32, pady=8)
+start_button.pack(side="left", padx=16, pady=8)
 
 stop_button = tk.Button(root, text="Stop", command=stop_script)
-stop_button.pack(side="right", padx=32, pady=8)
+stop_button.pack(side="right", padx=16, pady=8)
 
 root.mainloop()
